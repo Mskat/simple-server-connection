51c023dbef4bae8df73551585c058ee2044b373a
unknown <zur.katarzyna@gmail.com>
2019-12-31 17:33:03 +0000

    Create objects to handle input and output streams.
    Changed variable serverSocket from instance variable to local.
    For input stream, created an instance of BufferedReaer class, and for output created an object of PrintWriter class.
    Created while loop to get and display messages.
    
    i Please enter the commit message for your changes. Lines starting

--- src/main/java/com/server/Server.java
+++ src/main/java/com/server/Server.java
@@ -1,12 +1,13 @@
 package com.server;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server {
-
-    private ServerSocket serverSocket;
 
     public static void main(String[] args) {
         Server myServer = new Server(5000);
@@ -14,10 +15,17 @@ public class Server {
 
     public Server(int port) {
         try {
-            serverSocket = new ServerSocket(port);
+            ServerSocket serverSocket = new ServerSocket(port);
             System.out.println("Server is listening on port: " + port);
             Socket clientSocket = serverSocket.accept();
             System.out.println("Client accepted");
+            PrintWriter output = new PrintWriter(clientSocket.getOutputStream(), true);
+            BufferedReader input = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+            String userInput;
+            while((userInput = input.readLine()) != null) {
+                output.println("Server received message: " + userInput);
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
